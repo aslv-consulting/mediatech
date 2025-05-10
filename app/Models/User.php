6808c04d78f216a6b486470b5267a328b6a3e9e0
slucienvauthier-aslv <99436847+slucienvauthier-aslv@users.noreply.github.com>
2025-05-10 17:20:11 +0200

    feat: Configurer inteliphence et pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,18 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
 
 class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
